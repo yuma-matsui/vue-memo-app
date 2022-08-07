2c972357e557da394b4e9bc83ce81bd1f1789a53
yuma-matsui <oshino.iiyo@gmail.com>
2022-08-07 22:33:14 +0900

    メモの新規作成用コンポーネント作成

---
+++ src/components/MemoCreateForm.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="create-form-wrapper">
+    <h2>メモを追加する</h2>
+    <form @submit.prevent>
+      <div class="input-container">
+        <label for="title">タイトル</label>
+        <input
+          type="text"
+          id="title"
+          placeholder="Input the title of this memo"
+          v-model="newMemo.title"
+        >
+      </div>
+      <div class="input-container">
+        <label for="description">詳細</label>
+        <textarea
+          id="description"
+          placeholder="Input the description of this memo"
+          v-model="newMemo.description"
+        ></textarea>
+      </div>
+      <input
+        type="submit"
+        value="作成"
+        @click="createMemo"
+      >
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      newMemo: {
+        title: '',
+        description: '',
+        editable: false
+      }
+    }
+  },
+
+  methods: {
+    createMemo () {
+      this.$store.dispatch('createMemo', { ...this.newMemo })
+      this.resetNewMemo()
+    },
+
+    resetNewMemo () {
+      this.newMemo.title = ''
+      this.newMemo.description = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+h2 {
+  margin: 0;
+}
+
+.create-form-wrapper {
+  margin: 50px auto;
+}
+
+.input-container {
+  margin: 10px 0px;
+  display: flex;
+  flex-direction: column;
+}
+
+.input-container input {
+  padding: 5px;
+  display: inline-block;
+  width: 15%;
+  align-self: center;
+  border: solid 1px black;
+  border-radius: 3px;
+}
+
+.input-container textarea {
+  padding: 5px;
+  display: inline-block;
+  width: 25%;
+  align-self: center;
+  border: solid 1px black;
+  border-radius: 3px;
+}
+
+input[type="submit"] {
+  display: inline-block;
+  border: solid 1px black;
+  border-radius: 5px;
+  background-color: black;
+  color: white;
+  padding: 3px 12px;
+  cursor: pointer;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,34 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    memos: JSON.parse(localStorage.getItem('memos')) || []
   },
   getters: {
+    existMemos (state) {
+      return state.memos.length > 0
+    },
+
+    subTitle (_, getters) {
+      return getters.existMemos
+        ? 'Check your memos below.'
+        : "Let's write your memos."
+    }
   },
   mutations: {
+    createMemo (state, newMemo) {
+      state.memos.push(newMemo)
+    },
+
+    saveMemos (state) {
+      localStorage.clear()
+      const memosJSON = JSON.stringify(state.memos)
+      localStorage.setItem('memos', memosJSON)
+    }
   },
   actions: {
-  },
-  modules: {
+    createMemo ({ commit }, newMemo) {
+      commit('createMemo', newMemo)
+      commit('saveMemos')
+    }
   }
 })
