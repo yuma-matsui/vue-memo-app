427902161f0e3079581c79af6f041363fdaa09fb
yuma-matsui <oshino.iiyo@gmail.com>
2022-08-07 22:31:35 +0900

    メインコンポーネントの名前変更

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import MemoApp from '@/MemoApp'
+import router from '@/router'
+import store from '@/store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(MemoApp).use(store).use(router).mount('#app')
