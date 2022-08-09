6ebc4ebb3bc372a1eff7f778b3e2ab84c29999f8
yuma-matsui <oshino.iiyo@gmail.com>
2022-08-09 20:30:01 +0900

    修正#メモ個別ページコンポーネントにメモが存在しない場合の分岐追加

--- src/views/MemoDetails.vue
+++ src/views/MemoDetails.vue
@@ -1,13 +1,16 @@
 <template>
   <memo-app-top-title />
-  <memo-item
-    :index="index"
-  />
+  <template v-if="hasMemos">
+    <memo-item
+      :index="index"
+    />
+  </template>
 </template>
 
 <script>
 import MemoAppTopTitle from '@/components/MemoAppTopTitle'
 import MemoItem from '@/components/MemoItem'
+import { mapGetters } from 'vuex'
 
 export default {
   components: {
@@ -20,6 +23,12 @@ export default {
       type: String,
       required: true
     }
+  },
+
+  computed: {
+    ...mapGetters([
+      'hasMemos'
+    ])
   }
 }
 </script>
